cb97689fedc1dc608c2450874511240fc112a265
Pedro Rocha <pedrovqrocha@gmail.com>
2023-09-23 23:10:24 +0200

    feat(main-loop): main loop to repeat questions + formatting commute days

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
 	"time"
 
 	"github.com/AlecAivazis/survey/v2"
@@ -34,6 +35,27 @@ func (days CommuteDateSlice) getDisplayTitles() []string {
 		displayOptions = append(displayOptions, days[i].formatToRegister())
 	}
 	return displayOptions
+}
+
+func getTransportEmoji(transport string) string {
+	switch transport {
+	case "Bike":
+		return "🚲"
+	case "Public Transport":
+		return "🚈"
+	default:
+		return ""
+	}
+}
+
+func displayCommutes(commutes []CommuteEntry) {
+	for _, commute := range commutes {
+		fmt.Printf("Date: %s | Transport: %s %s\n",
+			commute.Date,
+			getTransportEmoji(commute.Transport),
+			commute.Transport,
+		)
+	}
 }
 
 // getWeekdayOptions calculates all the weekdays according to the current
@@ -70,60 +92,63 @@ func main() {
 		return
 	}
 
-	mainLoopOption := ""
-	mainLoopPrompt := &survey.Select{
-		Message: "What do you want to do?",
-		Options: []string{"Check commute days", "Register new commute", "Exit"},
-	}
-	survey.AskOne(mainLoopPrompt, &mainLoopOption)
-
-	switch mainLoopOption {
-	case "Exit":
-		os.Exit(0)
-	case "Check commute days":
-		for i, commute := range commutes {
-			fmt.Println(i, commute)
+	for {
+		mainLoopOption := ""
+		mainLoopPrompt := &survey.Select{
+			Message: "What do you want to do?",
+			Options: []string{"Check commute days", "Register new commute", "Exit"},
 		}
-	case "Register new commute":
-		days := []string{}
-		var weekdayDateOptions CommuteDateSlice = getWeekdayOptions()
-		daysPrompt := &survey.MultiSelect{
-			Message: "Which days do you want to register?",
-			Options: weekdayDateOptions.getDisplayTitles(),
-			Description: func(value string, index int) string {
-				return weekdayDateOptions[index].formatToDisplay()
-			},
-		}
-		survey.AskOne(daysPrompt, &days, survey.WithValidator(survey.Required))
+		survey.AskOne(mainLoopPrompt, &mainLoopOption)
 
-		transport := ""
-		transportPrompt := &survey.Select{
-			Message: "Choose the transportation:",
-			Options: getTransportationOptions(),
-		}
-		survey.AskOne(transportPrompt, &transport, survey.WithValidator(survey.Required))
+		switch mainLoopOption {
+		case "Exit":
+			os.Exit(0)
+		case "Check commute days":
+			displayCommutes(commutes)
+		case "Register new commute":
+			days := []string{}
+			var weekdayDateOptions CommuteDateSlice = getWeekdayOptions()
+			daysPrompt := &survey.MultiSelect{
+				Message: "Which days do you want to register?",
+				Options: weekdayDateOptions.getDisplayTitles(),
+				Description: func(value string, index int) string {
+					return weekdayDateOptions[index].formatToDisplay()
+				},
+			}
+			survey.AskOne(daysPrompt, &days, survey.WithValidator(survey.Required))
 
-		fmt.Printf("%s - %s\n", days, transport)
+			transport := ""
+			transportPrompt := &survey.Select{
+				Message: "Choose the transportation:",
+				Options: getTransportationOptions(),
+			}
+			survey.AskOne(transportPrompt, &transport, survey.WithValidator(survey.Required))
 
-		for _, day := range days {
-			newEntry := CommuteEntry{
-				Date:      day,
-				Transport: transport,
+			fmt.Printf("%s - %s\n", days, transport)
+
+			for _, day := range days {
+				newEntry := CommuteEntry{
+					Date:      day,
+					Transport: transport,
+				}
+
+				commutes = append(commutes, newEntry)
+			}
+			sort.Slice(commutes, func(i, j int) bool {
+				return commutes[i].Date > commutes[j].Date
+			})
+
+			updatedData, err := json.MarshalIndent(commutes, "", " ")
+			if err != nil {
+				fmt.Println("Error registering new commute:", err)
+				return
 			}
 
-			commutes = append(commutes, newEntry)
-		}
-
-		updatedData, err := json.MarshalIndent(commutes, "", " ")
-		if err != nil {
-			fmt.Println("Error registering new commute:", err)
-			return
-		}
-
-		err = os.WriteFile(filePath, updatedData, os.ModePerm)
-		if err != nil {
-			fmt.Println("Could not write into the file:", err)
-			return
+			err = os.WriteFile(filePath, updatedData, os.ModePerm)
+			if err != nil {
+				fmt.Println("Could not write into the file:", err)
+				return
+			}
 		}
 	}
 }
